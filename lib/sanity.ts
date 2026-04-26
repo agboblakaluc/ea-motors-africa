1e34e62a2cee3eba7e2f3c50e035d2fdbcd2dac4
Luc Agboblaka <agboblakaluc@gmail.com>
2026-04-26 17:11:00 +0000

    fix: hardcode sanity projectId fallback for build

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from '@sanity/image-url'
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'ceiyi7wh',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01',
   useCdn: true,
